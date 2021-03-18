b17a66d0503a1fd52f8c62014164cd5f321fa7cb
Marxist-Leninist <31905382+Marxist-Leninist@users.noreply.github.com>
2021-03-18 07:35:39 +0000

    print line changes

--- StellarisSettingRandomizer.jl
+++ StellarisSettingRandomizer.jl
@@ -1,11 +1,15 @@
 #simple verion with only ordinal numbers
 println()#fixes initalization problem when Julia is first loaded sometime firt lines don't print
-println("Galaxy Size: ")
+print("Galaxy Size: ")
 println(rand()*1000)
-println("Galaxy shape (Elliptical as 1 going right): ")
+print("Galaxy shape (Elliptical as 1 going right): ")
 println(round(rand()*4, digits=0))
-println("AI Empires(using the right hand number): ")#31 options 0-30
+print("AI Empires(using the right hand number): ")#31 options 0-30
 println(round(rand()*30, digits=0))
 
-println("Advanced AI Starts:") #31 option off to 30
+print("Advanced AI Starts: ") #31 option off to 30
 println(round(rand()*30, digits=0))
+print("Fallen Empires: ")
+println(round(rand()*5, digits=0)) #6 option off to 5
+print("Marauder Empire: ")
+println(round(rand()*3, digits=0)) #4 option off to 4
