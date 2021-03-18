80ebfcde690813ffdcfb86451e45bf43eabeca8c
Marxist-Leninist <31905382+Marxist-Leninist@users.noreply.github.com>
2021-03-18 06:59:55 +0000

    Galaxy shape v1

---
+++ StellarisSettingRandomizer.jl
@@ -0,0 +1,7 @@
+#simple verion with only ordinal numbers
+println()#fixes initalization problem when Julia is first loaded sometime firt lines don't print
+println("Galaxy Size: ")
+println(rand()*1000)
+println("Galaxy shape: (Elliptical as 1 going right)")
+println(round(rand()*4, digits=0))
+println("AI Empires: ")#31 options 0-31
