8166c90a88966aaa13543bd60505793f5bc4c8d4
Marxist-Leninist <31905382+Marxist-Leninist@users.noreply.github.com>
2021-03-18 07:20:33 +0000

    Advanced AI starts

--- StellarisSettingRandomizer.jl
+++ StellarisSettingRandomizer.jl
@@ -2,6 +2,10 @@
 println()#fixes initalization problem when Julia is first loaded sometime firt lines don't print
 println("Galaxy Size: ")
 println(rand()*1000)
-println("Galaxy shape: (Elliptical as 1 going right)")
+println("Galaxy shape (Elliptical as 1 going right): ")
 println(round(rand()*4, digits=0))
-println("AI Empires: ")#31 options 0-31
+println("AI Empires(using the right hand number): ")#31 options 0-30
+println(round(rand()*30, digits=0))
+
+println("Advanced AI Starts:") #31 option off to 30
+println(round(rand()*30, digits=0))
